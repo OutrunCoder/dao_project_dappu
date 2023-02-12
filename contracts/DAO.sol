51e01c34ad968c5ecbc027ea7432bffeae35dc3a
David Pineda <david.pineda@deparadise.com>
2023-02-11 22:04:37 -0500

    start the constructor of the DAO contract

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -2,8 +2,24 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";
+
+struct DAODeploymentArgs {
+  Token _tokenContractAddress;
+  uint256 quorum;
+}
 
 contract DAO {
-    
+    // STATE
+    address owner;
+    Token public tokenContract;
+    uint256 public quorum;
+
+    constructor(DAODeploymentArgs memory args) {
+      owner = msg.sender;
+      tokenContract = args._tokenContractAddress;
+      quorum = args._quorum;
+    }
+
 
 }
