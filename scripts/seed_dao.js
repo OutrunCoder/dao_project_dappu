037fce5cdfba161df3cb5261060e861606944bb7
David Pineda <david.pineda@deparadise.com>
2023-02-18 04:56:01 -0500

    actor accounts

--- scripts/seed_dao.js
+++ scripts/seed_dao.js
@@ -8,6 +8,23 @@ const tokensToWei = (n) => {
 const etherToWei = tokensToWei;
 
 async function main() {
+  console.log('>> Fetch accouts and network...\n');
+
+  const accounts = await hre.ethers.getSigners();
+    // ACTORS
+    const [
+      deployer,
+      funder,
+      //
+      investor_1,
+      investor_2,
+      investor_3,
+      investor_4,
+      investor_5,
+      //
+      recipient_1,
+      randomUser
+    ] = accounts;
 }
 
 main().catch((error) => {
