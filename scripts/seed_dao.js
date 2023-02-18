cbf1fa187bab3dadc1a8f6793f7801b85e0f646c
David Pineda <david.pineda@deparadise.com>
2023-02-18 05:20:56 -0500

    Distribute governance tokens

--- scripts/seed_dao.js
+++ scripts/seed_dao.js
@@ -28,7 +28,26 @@ async function main() {
 
   console.log('>> Fetch Token contract... \n');
   const tokenContract = await hre.ethers.getContractAt('Token', '0x5FbDB2315678afecb367f032d93F642f64180aa3');
-  console.log(`Token fetched: ${tokenContract.address}`);
+  console.log(`Token fetched: ${tokenContract.address}\n`);
+
+  console.log('>> Distribute to investors... \n');
+  // TODO - BRING IN ASYNC AWAIT LIB SERIES MANAGEMENT
+  // DIST DAO TOKENS TO INVESTORS
+  // [].forEach((investor) => {});
+  // REFACTOR into async series >>>
+  let distTrx;
+  const distDAO_tokenAmount = tokensToWei(200000); // = 20%
+  distTrx = await tokenContract.transfer(investor_1.address, distDAO_tokenAmount);
+  await distTrx.wait();
+  distTrx = await tokenContract.transfer(investor_2.address, distDAO_tokenAmount);
+  await distTrx.wait();
+  distTrx = await tokenContract.transfer(investor_3.address, distDAO_tokenAmount);
+  await distTrx.wait();
+  // distTrx = await tokenContract.transfer(investor_4.address, distDAO_tokenAmount);
+  // await distTrx.wait();
+  // distTrx = await tokenContract.transfer(investor_5.address, distDAO_tokenAmount);
+  // await distTrx.wait();
+  // REFACTOR <<<
 }
 
 main().catch((error) => {
