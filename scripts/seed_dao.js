5d5d663f253de55856289ddfaea33f40211957d9
David Pineda <david.pineda@deparadise.com>
2023-02-18 05:10:03 -0500

    tab spacing

--- scripts/seed_dao.js
+++ scripts/seed_dao.js
@@ -11,20 +11,20 @@ async function main() {
   console.log('>> Fetch accouts and network...\n');
 
   const accounts = await hre.ethers.getSigners();
-    // ACTORS
-    const [
-      deployer,
-      funder,
-      //
-      investor_1,
-      investor_2,
-      investor_3,
-      investor_4,
-      investor_5,
-      //
-      recipient_1,
-      randomUser
-    ] = accounts;
+  // ACTORS
+  const [
+    deployer,
+    funder,
+    //
+    investor_1,
+    investor_2,
+    investor_3,
+    investor_4,
+    investor_5,
+    //
+    recipient_1,
+    randomUser
+  ] = accounts;
 }
 
 main().catch((error) => {
