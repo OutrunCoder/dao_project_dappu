fd1256abb2d38f8266286c3807c9e3559ddc6aeb
David Pineda <david.pineda@deparadise.com>
2023-02-19 18:05:38 -0500

    add form controls and submit button

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,10 +5,11 @@ import { ethers } from "ethers";
 const Create = ({ provider, daoContract, setIsloading }) => {
   return (
     <Form>
-      <Form.Group>
-        <FormControl type="text"/>
-        <FormControl type="number"/>
-        <FormControl type="text"/>
+      <Form.Group style={{ maxWidth: '500px', margin: '50px auto'}}>
+        <FormControl type="text" placeholder="Enter name" className='my-2'/>
+        <FormControl type="number" placeholder="Enter amount" className='my-2'/>
+        <FormControl type="text" placeholder="Enter address" className='my-2'/>
+        <Button variant="primary" type="submit" style={{ width: '100%'}}> Create Proposal</Button>
       </Form.Group>
     </Form>
   );
