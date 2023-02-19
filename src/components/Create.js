73d1646227da50f525114d61c52a0890835b3fb7
David Pineda <david.pineda@deparadise.com>
2023-02-19 18:13:15 -0500

    add handleCreateProposal

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,8 +3,21 @@ import { Form, Button, Spinner, FormControl } from "react-bootstrap";
 import { ethers } from "ethers";
 
 const Create = ({ provider, daoContract, setIsloading }) => {
+  const handleCreateProposal = (e) => {
+    e.preventDefault();
+    console.log(`>> Creating Proposal...`);
+
+    try {
+
+      // setIsloading(true);
+    } catch(err) {
+      console.error('(!) CREATE PROPOSAL FAILED:', err);
+      window.alert('(!) User rejected or transaction reverted. (!)');
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleCreateProposal}>
       <Form.Group style={{ maxWidth: '500px', margin: '50px auto'}}>
         <FormControl type="text" placeholder="Enter name" className='my-2'/>
         <FormControl type="number" placeholder="Enter amount" className='my-2'/>
