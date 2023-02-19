db48c3d671a8f142e394c9cdb1e7fc7ad5705886
David Pineda <david.pineda@deparadise.com>
2023-02-19 18:21:19 -0500

    wire up form event and data collection

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,9 +3,14 @@ import { Form, Button, Spinner, FormControl } from "react-bootstrap";
 import { ethers } from "ethers";
 
 const Create = ({ provider, daoContract, setIsloading }) => {
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [recipient, setRecipient] = useState('');
+
   const handleCreateProposal = (e) => {
     e.preventDefault();
     console.log(`>> Creating Proposal...`);
+    console.table({ name, amount, recipient});
 
     try {
 
@@ -19,9 +24,24 @@ const Create = ({ provider, daoContract, setIsloading }) => {
   return (
     <Form onSubmit={handleCreateProposal}>
       <Form.Group style={{ maxWidth: '500px', margin: '50px auto'}}>
-        <FormControl type="text" placeholder="Enter name" className='my-2'/>
-        <FormControl type="number" placeholder="Enter amount" className='my-2'/>
-        <FormControl type="text" placeholder="Enter address" className='my-2'/>
+        <FormControl
+          type="text"
+          placeholder="Enter name"
+          className='my-2'
+          onChange={(e) => setName(e.target.value)}
+          />
+        <FormControl
+          type="number"
+          placeholder="Enter amount"
+          className='my-2'
+          onChange={(e) => setAmount(e.target.value)}
+          />
+        <FormControl
+          type="text"
+          placeholder="Enter address"
+          className='my-2'
+          onChange={(e) => setRecipient(e.target.value)}
+          />
         <Button variant="primary" type="submit" style={{ width: '100%'}}> Create Proposal</Button>
       </Form.Group>
     </Form>
